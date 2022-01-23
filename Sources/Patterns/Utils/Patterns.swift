8d18d84f45414cbb115bbab350aded7146aee0fd
Vitaliy Zarubin <keygenqt@gmail.com>
2022-01-23 16:17:27 +0300

    update cli

---
+++ Sources/Patterns/Utils/Patterns.swift
@@ -0,0 +1,37 @@
+//
+//  Patterns.swift
+//  
+//
+//  Created by Виталий Зарубин on 23.01.2022.
+//
+
+import Foundation
+
+enum Patterns: String, CaseIterable {
+    // Behavioral
+    case chainOfResponsibility = "Chain of Responsibility"
+    case command = "Command"
+    case interpreter = "Interpreter"
+    case iterator = "Iterator"
+    case mediator = "Mediator"
+    case memento = "Memento"
+    case observer = "Observer"
+    case state = "State"
+    case strategy = "Strategy"
+    case templateMethod = "Template Method"
+    case visitor = "Visitor"
+    // Creational
+    case abstractFactory = "Abstract Factory"
+    case builder = "Builder"
+    case factoryMethod = "Factory Method"
+    case prototype = "Prototype"
+    case singleton = "Singleton"
+    // Structural
+    case adapter = "Adapter"
+    case bridge = "Bridge"
+    case composite = "Composite"
+    case decorator = "Decorator"
+    case facade = "Facade"
+    case flyweight = "Flyweight"
+    case proxy = "Proxy"
+}
--- Sources/Patterns/main.swift
+++ Sources/Patterns/main.swift
@@ -1,15 +1,62 @@
+//
+//  Patterns.swift
+//
+//
+//  Created by Виталий Зарубин on 23.01.2022.
+//
+
 import SwiftCLI
 
-class GreetCommand: Command {
-    let name = "greet"
+let description = """
+Examples of Reusable Design Patterns:
+* "\(Patterns.chainOfResponsibility.rawValue)"
+* "\(Patterns.command.rawValue)"
+* "\(Patterns.interpreter.rawValue)"
+* "\(Patterns.iterator.rawValue)"
+* "\(Patterns.mediator.rawValue)"
+* "\(Patterns.memento.rawValue)"
+* "\(Patterns.observer.rawValue)"
+* "\(Patterns.state.rawValue)"
+* "\(Patterns.strategy.rawValue)"
+* "\(Patterns.templateMethod.rawValue)"
+* "\(Patterns.visitor.rawValue)"
+* "\(Patterns.abstractFactory.rawValue)"
+* "\(Patterns.builder.rawValue)"
+* "\(Patterns.factoryMethod.rawValue)"
+* "\(Patterns.prototype.rawValue)"
+* "\(Patterns.singleton.rawValue)"
+* "\(Patterns.adapter.rawValue)"
+* "\(Patterns.bridge.rawValue)"
+* "\(Patterns.composite.rawValue)"
+* "\(Patterns.decorator.rawValue)"
+* "\(Patterns.facade.rawValue)"
+* "\(Patterns.flyweight.rawValue)"
+* "\(Patterns.proxy.rawValue)"
+"""
 
-    @Param var person: String
+func validate() -> [String] {
+    return Patterns.allCases.map { $0.rawValue }
+}
+
+class AppCommand: Command {
+    
+    let name: String = "-p"
+
+    @Param var patterns: String
+    
+    let percentage = Input.readLine(
+        prompt: "Pattern:",
+        // validation: [.contains(Patterns.allCases.map { $0.rawValue })],
+        errorResponse: { (input, reason) in
+            Term.stderr <<< "'\(input)' is invalid; must be a number between 0 and 100"
+        }
+    )
 
     func execute() throws {
-        stdout <<< "Hello \(person)!"
+        stdout <<< "Hello \(patterns)!"
     }
 }
 
-let greeter = CLI(name: "greeter")
-greeter.commands = [GreetCommand()]
+let greeter = CLI(name: "patterns", version: "patterns 1.0", description: description)
+greeter.commands = [AppCommand()]
 greeter.go()
